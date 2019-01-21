d14c0eb623eefca87d86142be92abee9c311b4b7
Luis4754 <46898143+Luis4754@users.noreply.github.com>
2019-01-21 09:43:20 -0600

    Luis Fernando Duran Castillo, Grupo 2

---
+++ velocidad.py
@@ -0,0 +1,10 @@
+# Autor: Luis Duran
+# Calcula la velocidad promedio de un viaje
+
+
+t= int(input("dame el tiempo del viaje en horas: "))
+d= int(input("dame la  distancia del viaje km enteros: "))
+
+v= d/t
+
+print ("La velocidad promedio es: ", v, "km/h")
